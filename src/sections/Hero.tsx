fa5cc79c57c2f390224dcb06349e2479e623189a
NewRishad <rishadhussein90@gmail.com>
2025-02-21 20:24:54 +0300

    fix: typo in hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,12 +6,12 @@ export const HeroSection = () => {
     <div className="py-32">
       <div className="container">
         <div className="flex flex-col items-center">
-        <Image src={memojiImage} className="size-[100px" alt="Person peeking from behind laptop" />
+        <Image src={memojiImage} className="size-[100px]" alt="Person peeking from behind laptop" />
         <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
           <div className="bg-green-500 size-2.5 rounded-full"></div>
           <div className="text-sm font-medium">Available for new projects</div>
           </div>
-          <h1 className="font-serif text-center mt-8 tracking-wide">Building Exceptional User Experiences</h1>
+          <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">Building Exceptional User Experiences</h1>
           <p className="mt-4 text-center text-white/60">
             I specialize in transforming designs into functional,
             high-performing web applications. Let's discuss your next project.
