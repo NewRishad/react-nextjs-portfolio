8476c539675e5f1213f2353e78c2a4b615b74458
NewRishad <rishadhussein90@gmail.com>
2025-02-22 22:30:31 +0300

    style(Hero): Add decorative stars, sparkles and circle icons for visual enhancements

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -3,14 +3,24 @@ import { PropsWithChildren } from "react";
 export const HeroOrbit = ({
   children,
   size,
-}: PropsWithChildren<{ size: number }>) => {
+  rotation,
+}: PropsWithChildren<{ size: number; rotation: number }>) => {
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
       <div
-        className=" border border-red-500"
-        style={{ height: `${size}px`, width: `${size}px` }}
+        className="flex items-start justify-start"
+        style={{
+          transform: `rotate(${rotation}deg)`,
+          height: `${size}px`,
+          width: `${size}px`,
+        }}
       >
-        <div className="border border-red-500 inline-flex">{children}</div>
+        <div
+          className="inline-flex"
+          style={{ transform: `rotate(${rotation * -1}deg)` }}
+        >
+          {children}
+        </div>
       </div>
     </div>
   );
